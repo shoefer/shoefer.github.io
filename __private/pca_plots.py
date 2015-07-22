989b6a20bf48161ea50191b93a15f04f63aebe7b
shoefer <sebastian.hoefer@tu-berlin.de>
2015-07-22 23:51:43 +0200

    adding drafts. working on plotting script for pca

---
+++ __private/pca_plots.py
@@ -0,0 +1,67 @@
+"""
+PCA Tutorial
+"""
+import numpy as np
+from numpy import *
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as p3
+import matplotlib.animation as animation
+from mpl_toolkits.mplot3d import Axes3D
+
+n = 50
+theta = np.linspace(-2 * np.pi, 2 * np.pi, n)
+X = np.zeros( (n, 3) ) 
+X[:,0] = theta #np.linspace(-2, 2, 100)
+X[:,1] = 3*np.sin(theta)
+X[:,2] = 0.
+
+Rx = lambda th: np.array([
+ [1, 0, 0], [0, cos(th), -sin(th)], [0, sin(th), cos(th)]
+])
+Ry = lambda th: np.array([
+ [0, 1, 0], [cos(th), 0, -sin(th)], [sin(th), 0, cos(th)]
+])
+
+Xrot = X.dot(Rx(pi/2.).T).dot(Rx(pi/8).T)
+
+# original
+# fig = plt.figure()
+# ax = Axes3D(fig)
+# ax.scatter(X[:,0],X[:,1],X[:,2], marker='o', s=20, c="blue", alpha=0.6)
+
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.scatter(Xrot[:,0],Xrot[:,1],Xrot[:,2], marker='o', s=20, c="blue", alpha=0.6)
+
+# compute PCA
+from sklearn.decomposition import PCA
+pca = PCA(n_components=3)
+pca.fit(Xrot)
+
+# plane
+normal = pca.components_[-1]
+d = -np.array([0,0,0]).dot(normal)
+# create x,y
+# xx, yy = np.meshgrid(theta, theta)
+xx, yy = np.meshgrid(linspace(theta[0],theta[-1],5), linspace(theta[0]/2,theta[-1]/2,5))
+
+# calculate corresponding z
+z = (-normal[0] * xx - normal[1] * yy - d) * 1. /normal[2]
+
+# plot the surface
+#plt3d = plt.figure().gca(projection='3d')
+ax.plot_surface(xx, yy, z, alpha=0.3)
+
+ax.set_xlim3d([theta[0],theta[-1]])
+ax.set_ylim3d([theta[0],theta[-1]])#([-1,1])
+ax.set_zlim3d([theta[0],theta[-1]])#([-1,1])
+
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+
+plt.show()
+
+# for ii in xrange(0,45,1):
+# 	ax.view_init(elev=10., azim=ii)
+# 	plt.savefig("movie%d.png" % ii)
---
+++ _drafts/2015-07-21-mean-variance.markdown
@@ -0,0 +1,21 @@
+---
+layout: intuds_post
+title:  "Mean and Variance"
+date:   2015-07-21 15:00:00
+categories: intuds
+comments: true
+---
+
+http://mathoverflow.net/questions/1048/why-is-it-so-cool-to-square-numbers-in-terms-of-finding-the-standard-deviation
+
+In the last article we have come up with a definition of data: Data is numbers + the knowledge what these numbers mean + an instruction of how to convert between the numbers and the actual thing that the numbers represent. But we have also said that we want to do something with data. This is where functions come into play.
+
+## Mathematical Functions
+
+
+### [TL;DR](http://de.urbandictionary.com/define.php?term=tl%3Bdr):
+- 
+
+### <a name="further"></a>Footnotes:
+1. <a name="[1]"></a>Of course this is largely oversimplified. The actual price would also depend on whether it is close to the city center or in the suburb, whether it is a new apartment or a shabby one, etc. But for the sake of the example let's assume none of these factors plays a role.
+2. <a name="[2]"></a>To cite the [mathematician Ian Stewart](http://books.google.de/books?id=dUhMAQAAQBAJ&pg=PA182&lpg=PA182&dq=Classical+mathematics+concentrated+on+linear+equations+for+a+sound+pragmatic+reason:+it+could+not+solve+anything+else.&source=bl&ots=PuRT666z3D&sig=YBZtoUP_y0siL0RUXfC14keMGe4&hl=de&sa=X&ei=upteVPDfBIysPJChgZgE&ved=0CCsQ6AEwAQ#v=onepage&q=Classical%20mathematics%20concentrated%20on%20linear%20equations%20for%20a%20sound%20pragmatic%20reason%3A%20it%20could%20not%20solve%20anything%20else.&f=false): "Classical mathematics concentrated on linear equations for a sound pragmatic reason: it could not solve anything else."
